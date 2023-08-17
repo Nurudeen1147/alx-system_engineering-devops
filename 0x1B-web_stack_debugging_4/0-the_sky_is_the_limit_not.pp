8a850528d91e8f7d3d0506617593b2cd8fb65547
Nurudeen Oyelowo <neroevali4u@yahoo.co.uk>
2023-08-17 13:07:30 +0000

    Web S D 4

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# Fixing the nginx site that cannot handle multiple concurrent requests
+exec { 'fix--for-nginx':
+  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
+/etc/default/nginx; service nginx restart\"",
+  path    => '/usr/bin:/usr/sbin:/bin'
+}
